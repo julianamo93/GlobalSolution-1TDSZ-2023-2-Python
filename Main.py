0a23b987ed66a4b2b14a6ffc6c057d0a3834262c
KevinNobre <kevinchristian.nobre@gmail.com>
2023-12-01 00:13:05 -0300

    first commit

---
+++ Main.py
@@ -0,0 +1,131 @@
+import datetime
+
+class Paciente:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        self.historico = []
+
+    def adicionar_evento(self, evento):
+        self.historico.append(evento)
+
+class EventoSaude:
+    def __init__(self, descricao, data):
+        self.descricao = descricao
+        self.data = data
+
+class Triagem:
+    def __init__(self, pressao_arterial, temperatura, sintomas, data):
+        self.pressao_arterial = pressao_arterial
+        self.temperatura = temperatura
+        self.sintomas = sintomas
+        self.data = data
+
+    def verificar_estado_saude(self):
+        if self.temperatura > 37.8:
+            print(f"Este paciente está com febre")
+            return "\nATENÇÃO: Este paciente precisa de atendimento médico! Dentro de alguns minutos um de nossos profissionais entrará em contato."
+
+class AyuCare:
+    def __init__(self):
+        self.pacientes = {}
+
+    def cadastrar_paciente(self, paciente):
+        self.pacientes[paciente.nome] = paciente
+
+    def realizar_triagem(self, paciente):
+        pressao_arterial = input(f"\nDigite a pressão arterial para {paciente.nome}: ")
+        temperatura = float(input(f"\nDigite a temperatura para {paciente.nome}: "))
+        sintomas = input(f"\nDescreva os sintomas para {paciente.nome}: ")
+        data_triagem = datetime.date.today()
+
+        triagem = Triagem(pressao_arterial, temperatura, sintomas, data_triagem)
+        paciente.adicionar_evento(triagem)
+
+        resultado_triagem = triagem.verificar_estado_saude()
+        print(f"\nTriagem realizada para paciente {paciente.nome}.\n {resultado_triagem}")
+
+    def monitorar_saude(self, paciente):
+        descricao = input(f"\nComo você está se sentindo hoje {paciente.nome}? ")
+        data = datetime.date.today()
+        evento = EventoSaude(descricao, data)
+        paciente.adicionar_evento(evento)
+        print(f"\nEvento de saúde registrado, o paciente se sente {descricao}")
+
+    def exibir_historico(self, paciente):
+        print(f"\nHistórico armazenado de {paciente.nome}:")
+        for evento in paciente.historico:
+            if isinstance(evento, Triagem):
+                print(f"\n--> Triagem <--")
+                print(f"\nData: {evento.data}\nPressão arterial: {evento.pressao_arterial}\nTemperatura: {evento.temperatura}\nSintomas: {evento.sintomas}")
+                resultado_triagem = evento.verificar_estado_saude()
+                if resultado_triagem:
+                    print(resultado_triagem)
+            elif isinstance(evento, EventoSaude):
+                print(f"Em {evento.data}: {paciente.nome} estava {evento.descricao}")
+
+# Função para coletar informações do paciente
+def obter_informacoes_paciente():
+    nome = input("\nDigite o nome do paciente que está sendo cadastrado: ")
+    idade = int(input("\nDigite a idade: "))
+    return Paciente(nome, idade)
+
+if __name__ == "__main__":
+    ayucare = AyuCare()
+
+    while True:
+        print("\n-------------------------------")
+        print("\n--- Diário de Saúde AyuCare ---")
+        print("\nLembre-se que o preenchimento do diário é de suma importância para que conheçamos você e sua rotina.\n")
+        print("1. Cadastrar Paciente")
+        print("2. Realizar Triagem")
+        print("3. Monitorar Saúde e Humor")
+        print("4. Exibir Histórico de um Paciente")
+        print("5. Sair")
+
+        escolha = input("\nDigite o número da opção desejada: ")
+
+        if escolha == "1":
+            novo_paciente = obter_informacoes_paciente()
+            ayucare.cadastrar_paciente(novo_paciente)
+        elif escolha == "2":
+            print("\n--- Lista de Pacientes ---")
+            for i, paciente in enumerate(ayucare.pacientes.values(), 1):
+                print(f"{i}. {paciente.nome}")
+            
+            escolha_paciente = int(input("\nEscolha o número do paciente para realizar a triagem: "))
+            pacientes = list(ayucare.pacientes.values())
+            
+            if 1 <= escolha_paciente <= len(pacientes):
+                ayucare.realizar_triagem(pacientes[escolha_paciente - 1])
+            else:
+                print("Opção inválida. Tente novamente.")
+        elif escolha == "3":
+            print("\n--- Lista de Pacientes ---")
+            for i, paciente in enumerate(ayucare.pacientes.values(), 1):
+                print(f"{i}. {paciente.nome}")
+            
+            escolha_paciente = int(input("\nEscolha o número do paciente para realizar o monitoramento: "))
+            pacientes = list(ayucare.pacientes.values())
+            
+            if 1 <= escolha_paciente <= len(pacientes):
+                ayucare.monitorar_saude(pacientes[escolha_paciente - 1])
+            else:
+                print("Opção inválida. Tente novamente.")
+        elif escolha == "4":
+            print("\n--- Lista de Pacientes ---")
+            for i, paciente in enumerate(ayucare.pacientes.values(), 1):
+                print(f"{i}. {paciente.nome}")
+        
+            escolha_paciente = int(input("\nEscolha o número do paciente para exibir o histórico: "))
+            pacientes = list(ayucare.pacientes.values())
+    
+            if 1 <= escolha_paciente <= len(pacientes):
+                ayucare.exibir_historico(pacientes[escolha_paciente - 1])
+            else:
+                print("Opção inválida. Tente novamente.")
+        elif escolha == "5":
+            print("Saindo do programa. Obrigado!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
